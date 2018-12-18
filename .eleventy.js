c45c48bed6d8343c1591dd54bf89b009f8d85a39
Max Böck <hello@mxb.at>
2018-12-18 21:26:39 +0100

    modify config

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ const htmlmin = require('html-minifier')
 
 module.exports = function(config) {
     config.addFilter('isoDate', require('./_custom/filters/isoDate.js'))
+    config.addNunjucksShortcode('icon', require('./_custom/shortcodes/icon.js'))
 
     config.addPlugin(pluginRss)
     config.addPlugin(pluginSyntaxHighlight)
@@ -11,8 +12,6 @@ module.exports = function(config) {
     config.addLayoutAlias('base', 'layouts/base.njk')
     config.addLayoutAlias('page', 'layouts/page.njk')
     config.addLayoutAlias('post', 'layouts/post.njk')
-
-    config.addNunjucksShortcode('icon', require('./_custom/shortcodes/icon.js'))
 
     config.addTransform('htmlmin', function(content, outputPath) {
         if (outputPath.endsWith('.html')) {
