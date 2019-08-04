1cc69f6fa6ba29d9f769a1fec3c05e3c8f1f2a94
Max Böck <hello@mxb.at>
2019-08-04 20:54:29 +0200

    basic lambda function

--- _lambda/publish.js
+++ _lambda/publish.js
@@ -1,7 +1,15 @@
+import querystring from 'querystring'
+
 // Main Lambda Function Handler
 exports.handler = async (event, context, callback) => {
+    // Only allow POST
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' }
+    }
+    const params = querystring.parse(event.body)
+
     callback(null, {
         statusCode: 200,
-        body: 'Publish Function'
+        body: `Publish: ${params.title}`
     })
 }
