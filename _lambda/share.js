0f2e9c2760e06e23660f15fe92d0f4b621b74534
Max Böck <hello@mxb.at>
2019-08-06 15:26:06 +0200

    update sharing form

--- _lambda/share.js
+++ _lambda/share.js
@@ -4,7 +4,7 @@ import querystring from 'querystring'
 exports.handler = async (event, context, callback) => {
     // Only allow POST
     if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' }
+        return callback(null, { statusCode: 405, body: 'Method Not Allowed' })
     }
     const params = querystring.parse(event.body)
 
