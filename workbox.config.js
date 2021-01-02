6c337b1286ed294dcf738228c1998a229ac58ec4
Max Böck <hello@mxb.at>
2021-01-02 16:10:45 +0100

    add workbox

---
+++ workbox.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    cacheId: 'mxb',
+    globDirectory: './dist',
+    globPatterns: ['**/*.{css,js,woff2}'],
+    swDest: './dist/sw.js',
+    sourcemap: false,
+    cleanupOutdatedCaches: true,
+    clientsClaim: true,
+    skipWaiting: true,
+    runtimeCaching: [
+        {
+            urlPattern: /\.(?:png|jpg|jpeg|gif|webp|svg)$/,
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'mxb-images',
+                expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 60 * 60 * 24 * 365
+                }
+            }
+        }
+    ]
+}
