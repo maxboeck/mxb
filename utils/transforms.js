48062b26e3c4e67dc0297ba3009170b818ca0817
Max Böck <hello@mxb.at>
2021-01-05 14:01:16 +0100

    rm critical dependencies

--- utils/transforms.js
+++ utils/transforms.js
@@ -1,13 +1,9 @@
 const htmlmin = require('html-minifier')
-// const critical = require('critical')
-const buildDir = 'dist'
 
 const shouldTransformHTML = (outputPath) =>
     outputPath &&
     outputPath.endsWith('.html') &&
     process.env.ELEVENTY_ENV === 'production'
-
-// const isHomePage = (outputPath) => outputPath === `${buildDir}/index.html`
 
 process.setMaxListeners(Infinity)
 module.exports = {
@@ -21,23 +17,4 @@ module.exports = {
         }
         return content
     }
-
-    // critical: async function (content, outputPath) {
-    //     if (shouldTransformHTML(outputPath) && isHomePage(outputPath)) {
-    //         try {
-    //             const config = {
-    //                 base: `${buildDir}/`,
-    //                 html: content,
-    //                 inline: true,
-    //                 width: 1280,
-    //                 height: 800
-    //             }
-    //             const { html } = await critical.generate(config)
-    //             return html
-    //         } catch (err) {
-    //             console.error(err)
-    //         }
-    //     }
-    //     return content
-    // }
 }
