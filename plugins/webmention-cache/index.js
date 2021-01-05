8a2c8825d723689ce451247a7ec34d352c508c01
Max Böck <hello@mxb.at>
2021-01-05 13:17:21 +0100

    rm keybase reference

--- plugins/webmention-cache/index.js
+++ plugins/webmention-cache/index.js
@@ -1,14 +1,14 @@
 module.exports = {
-    // Restore file/directory cached in previous builds.
+    // Restore a directory cached in previous builds.
     // Does not do anything if:
-    //  - the file/directory already exists locally
-    //  - the file/directory has not been cached yet
+    //  - the directory already exists locally
+    //  - the directory has not been cached yet
     async onPreBuild({ utils }) {
         await utils.cache.restore('./cache')
     },
-    // Cache file/directory for future builds.
+    // Cache directory for future builds.
     // Does not do anything if:
-    //  - the file/directory does not exist locally
+    //  - the directory does not exist locally
     async onPostBuild({ utils }) {
         await utils.cache.save('./cache')
     }
