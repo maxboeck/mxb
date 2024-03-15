58b4bb218e2d4004f4d8e7b34997627974da39e3
Max Böck <max.boeck@codista.com>
2024-03-15 14:53:34 +0100

    add css naked day check

--- src/data/build.js
+++ src/data/build.js
@@ -1,7 +1,16 @@
 const timestamp = new Date()
 
+function isCSSNakedDay() {
+    const now = Date.now()
+    const currentYear = new Date().getFullYear()
+    const startEpoch = new Date(`${currentYear}-04-09T00:00:00+1400`).getTime()
+    const endEpoch = new Date(`${currentYear}-04-09T23:59:59-1200`).getTime()
+    return startEpoch <= now && now <= endEpoch
+}
+
 module.exports = {
     env: process.env.ELEVENTY_ENV,
     timestamp: timestamp,
-    id: timestamp.valueOf()
+    id: timestamp.valueOf(),
+    naked: isCSSNakedDay()
 }
