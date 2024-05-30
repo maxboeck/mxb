f372d29424c87e63f084fd874e79ecefa8bee833
Max Böck <hello@mxb.at>
2024-05-30 14:02:49 +0200

    dynamic webmention script import

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,6 +1,10 @@
+// Module Preload
 import 'vite/modulepreload-polyfill'
+
+// Main Stylesheet
 import '../styles/main.scss'
 
+// Common Modules
 import './common/navigation'
 import './common/signup'
 import './common/lazyload'
@@ -9,3 +13,8 @@ import './common/infinitescroll'
 import './common/preload'
 import './common/speedlify'
 import './common/register-serviceworker'
+
+// Dynamic Modules, Imported at Runtime
+if (document.querySelector('#webmentions')) {
+    import('./webmentions')
+}
